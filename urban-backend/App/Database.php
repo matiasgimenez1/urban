a803a6072e72e78390a1437537716af3c976668c
matiasgimenez1 <gmatias95@hotmail.com>
2025-01-23 14:11:48 -0300

    Update Database.php

--- urban-backend/App/Database.php
+++ urban-backend/App/Database.php
@@ -4,8 +4,8 @@ namespace App;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 class Database {
-   // public function __construct() {
-     public function  __construct($username, $password) {  //matias
+    public function __construct() {
+     //public function  __construct($username, $password) {  //matias
     $capsule = new Capsule;
 
         $capsule->addConnection([
@@ -15,11 +15,11 @@ class Database {
             'host' => 'autorack.proxy.rlwy.net',
             'port' => '47478',                    
             'database' => 'railway',
-            //'username' => 'postgres',
+            'username' => 'postgres',
             //'password' => '123',
-            //'password' => 'EilJhYIdChoIIxhJKbIgqONEzCrCxDPw',
-            'username' => $username, // Usuario proporcionado desde el front matias
-            'password' => $password, // Contraseña proporcionada desde el front matias                    
+            'password' => 'EilJhYIdChoIIxhJKbIgqONEzCrCxDPw',
+            //'username' => $username, // Usuario proporcionado desde el front matias
+            //'password' => $password, // Contraseña proporcionada desde el front matias                    
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',

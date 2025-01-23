8c4bdf0e6bc870cdc6e200c4c344624bdaf12cd1
matiasgimenez1 <gmatias95@hotmail.com>
2025-01-23 11:36:57 -0300

    Update routes.php

--- urban-backend/routes/routes.php
+++ urban-backend/routes/routes.php
@@ -15,7 +15,7 @@ return function ($app) {
      $app->add(function ($req, $res, $next) {
          $response = $next($req, $res);
          return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Origin', 'https://urban-production-edbd.up.railway.app')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
      });

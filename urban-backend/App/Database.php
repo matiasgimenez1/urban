4ed26ab955f5e981068047930333844ab597d5b5
matiasgimenez1 <gmatias95@hotmail.com>
2025-01-24 22:34:51 -0300

    Update Database.php

--- urban-backend/App/Database.php
+++ urban-backend/App/Database.php
@@ -9,10 +9,11 @@ class Database {
 
         $capsule->addConnection([
             'driver' => 'pgsql',
-            'host' => 'localhost',
-            'database' => 'paintball',
+            'host' => 'autorack.proxy.rlwy.net',
+            'port' => '47478',                   
+            'database' => 'railway',
             'username' => 'postgres',
-            'password' => '123',
+            'password' => 'EilJhYIdChoIIxhJKbIgqONEzCrCxDPw',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
